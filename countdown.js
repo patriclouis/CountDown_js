33ec01bb1b8caa1236b905d61d42770df872165c
LI, PING <patriclouis.lee@gmail.com>
2013-10-03 21:16:58 +0800

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -7,12 +7,13 @@ function countdown()
 {
 	Deadline = new Date("2013/10/31 23:59:00")
 	today    = new Date() 
-	t_gap    = (Deadline.getTime()-today.getTime())
-	if (t_gap<=0)
+	t_gap    = Deadline.getTime() - today.getTime()
+	
+	if (t_gap <= 0)
 	{
-		if (t_gap<=0)
+		if (t_gap <= 0)
 		{
-			countdowndays.innerHTML = '0 days'
+                    countdowndays.innerHTML = '0 days'
 		    //alert('Deadline has passed!')
 		    return false
 		}
@@ -21,7 +22,7 @@ function countdown()
 	window.setTimeout("countdown()", 1000)
 
 	//sectime = t_gap/1000
-	//seconds = Math.floor(sectime)
+	//minsecs = Math.floor(sectime)
 
 	msPerDay= 24*60*60*1000 
 	e_days  = t_gap/msPerDay 
@@ -30,8 +31,8 @@ function countdown()
 	e_hours = (e_days - days)*24 
 	hours   = Math.floor(e_hours) 
 
-	e_mins  = (e_hours-hours)*60 
-	minutes = Math.floor((e_hours-hours)*60)
+	e_mins  = (e_hours - hours)*60 
+	minutes = Math.floor((e_hours - hours)*60)
 	seconds = Math.floor((e_mins -minutes)*60)
 
 	countdowndays.innerHTML = "<b><align=center><font color=ff0000>&nbsp;"+ days +
